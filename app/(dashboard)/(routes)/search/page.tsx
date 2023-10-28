627b752c9dd087ee1271409e3d09e565d23ae834
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-10-28 10:03:37 -0600

    🚀 feat: show categories list

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,6 +1,65 @@
-const SearchPage = () => {
+import { Categories } from '@/components/categories/categories'
+
+const SearchPage = async () => {
+  // const { userId } = auth();
+
+  // if (!userId) {
+  //   return redirect("/");
+  // }
+
+  // const categories = await db.category.findMany({
+  //   orderBy: {
+  //     name: "asc"
+  //   }
+  // });
+
+  // const courses = await getCourses({
+  //   userId,
+  //   ...searchParams,
+  // });
+
+
   return (
-    <div>SearchPage</div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        {/* <SearchInput /> */}
+      </div>
+      <div className="p-6 space-y-4">
+        <Categories
+          items={[
+            {
+              id: '1',
+              name: 'Music'
+            },
+            {
+              id: '2',
+              name: 'Photography'
+            },
+            {
+              id: '3',
+              name: 'Fitness'
+            },
+            {
+              id: '4',
+              name: 'Accounting'
+            },
+            {
+              id: '5',
+              name: 'Computer Science'
+            },
+            {
+              id: '6',
+              name: 'Filming'
+            },
+            {
+              id: '7',
+              name: 'Engineering'
+            },
+          ]}
+        />
+        {/* <CoursesList items={courses} /> */}
+      </div>
+    </>
   )
 }
 export default SearchPage
